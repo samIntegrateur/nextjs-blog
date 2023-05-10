dab1e14dcacb6bc9607e365fdf7863504da3aac1
Samuel Desbos <samuel-desbos@ext.connect-tech.sncf>
2023-05-10 15:34:36 +0200

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Layout from "../../components/Layout";
+import {getAllPostIds, getPostData} from "../../libs/posts";
+import Head from "next/head";
+import utilStyles from '../../styles/utils.module.css';
+import Date from "../../components/Date";
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id);
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export default function Post({ postData }) {
+    console.log("postData", postData);
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>
+    );
+}
